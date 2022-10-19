221fe2a0cb1129802d155b40b448c67b31b6184c
JenniferFariasRodrigues <jennifer.farias.rodrigues@gmail.com>
2022-10-18 21:14:18 -0400

    created service data

--- crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,18 +1,29 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
- propLegal = "qualquer";
-  constructor() { }
-
+//  propLegal = "qualquer";
+  constructor(private productService: ProductService,
+    private router :Router) { }
+    
   ngOnInit(): void {
   }
+  createProduct(): void {
+    this.productService.showMessage('Produto criado!');
 
-  fazerAlgo(): void {
-    console.log('Fazendo algo!');
   }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+
+  }
+
+  // fazerAlgo(): void {
+  //   console.log('Fazendo algo!');
+  // }
 }
